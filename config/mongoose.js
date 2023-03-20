765d8c2b1b50e49ac2f9f3a83c8b9368716d1434
Kamruzzaman <kzamanbn@gmail.com>
2023-03-20 21:52:15 +0600

    update mongoose@^7.0

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
 (async () => {
-	const connectionStr = process.env.MONGO_URI;
-	mongoose.set('strictQuery', true);
 	try {
-		await mongoose.connect(connectionStr, { useNewUrlParser: true });
+		await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 		console.log('MongoDB connected');
 	} catch (err) {
 		console.log(err);
