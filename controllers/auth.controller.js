d3aa3100822e83b07c5b1e91c576f9dbfcf5ea84
Kamruzzaman <kzamanbn@gmail.com>
2023-03-22 12:32:26 +0600

    update login cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -34,7 +34,8 @@ handler.login = async (req, res) => {
 				// set cookie
 				res.cookie(process.env.COOKIE_NAME, accessToken, {
 					maxAge: process.env.JWT_EXPIRY,
-					httpOnly: process.env.NODE_ENV === 'production',
+					httpOnly: true,
+					secure: process.env.NODE_ENV === 'production',
 					signed: true
 				});
 
