d1ad9fc32b85c16e1538c8bc3a66b018465ae6b2
Kamruzzaman <kzamanbn@gmail.com>
2023-04-15 11:20:06 +0600

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -35,7 +35,7 @@ handler.login = async (req, res) => {
 				res.cookie(process.env.COOKIE_NAME, accessToken, {
 					maxAge: process.env.JWT_EXPIRY,
 					httpOnly: !process.env.NODE_ENV === 'production',
-					secure: process.env.NODE_ENV === 'production',
+					secure: !process.env.NODE_ENV === 'production',
 					signed: true,
 					sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'lax'
 				});
